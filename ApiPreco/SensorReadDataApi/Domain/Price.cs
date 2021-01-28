0766299b507fcadddf5ce262af8ef9e63f4b1025
MATEUS LIMA DE CASTRO <mateuslimacastro_@hotmail.com>
2021-01-28 16:32:44 -0300

    Teste revert

--- ApiPreco/SensorReadDataApi/Domain/Price.cs
+++ ApiPreco/SensorReadDataApi/Domain/Price.cs
@@ -8,7 +8,7 @@ namespace PrecoApi.Domain
     public class Price
     {
         public long ProductId { get; set; }
-        public float SalePrice { get; set; }
+        public decimal SalePrice { get; set; }
         public decimal MaximumPrice { get; set; }
         public decimal PercentageDiscount { get; set; }
         public string DiscountType { get; set; }
